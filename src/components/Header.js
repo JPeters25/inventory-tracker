4a10fb2300401c05a63ffe238467480743ddfc96
Justin Peters <JMarvel525@gmail.com>
2022-09-15 15:25:46 -0700

    add background image and custom font

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 
 function Header(){
   const headerStyledComponentsStyles = {
+    fontFamily: 'EspressoBeans',
     textAlign: 'center'
   }
   return (
     <div style= {headerStyledComponentsStyles}>
-      <h1>Inventory Tracker</h1>
+      <h1>Coffee Tracker</h1>
     </div>
   );
 }
