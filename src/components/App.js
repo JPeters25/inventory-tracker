914b3cb0351632808c114d6d3244383191788e87
Justin Peters <JMarvel525@gmail.com>
2022-09-15 15:45:20 -0700

    add custom font to header component, style reusable form to center

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import CoffeeBagControl from "./CoffeeBagControl";
+import "./../App.css";
+
 
 function App(){
   return (
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -9,22 +9,32 @@ const ReusableForm = (props) => {
           type='text'
           name='names'
           placeholder='Coffee Name' />
+          <br/>
+          <br/>
         <input
           type='text'
           name='origin'
           placeholder='Coffee Origin' />
+          <br/>
+          <br/>
         <input
           type='text'
           name='price'
           placeholder='Price per Pound' />  
+          <br/>
+          <br/>
         <input
           type='text'
           name='roast'
           placeholder='Coffee Roast' /> 
+          <br/>
+          <br/>
         <input
           type='text'
           name='weight'
           placeholder='Bag Weight' /> 
+          <br/>
+          <br/>
         <button type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
